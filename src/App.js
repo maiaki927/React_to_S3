cb4122d0ab752e5a6865f985d486baf24f4a7533
maiaki927 <maiaki927@gmail.com>
2023-04-27 22:56:30 +0800

    change text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          anna github-action
+          anna github-action-test
         </p>
         <a
           className="App-link"
